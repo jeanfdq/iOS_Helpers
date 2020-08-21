00771d86a85a9150eb70d46c5568476cd71f82a4
Jean <jean.manzini@valid.com>
2020-08-21 12:59:13 -0300

    add extension TextField

--- Extensions.swift
+++ Extensions.swift
@@ -468,6 +468,25 @@ extension UILabel {
 
 extension UITextField {
     
+    public func addDoneCancelToolbar(onDone: (target: Any, action: Selector)? = nil, onCancel: (target: Any, action: Selector)? = nil) {
+        let onCancel = onCancel ?? (target: self, action: #selector(cancelButtonTapped))
+        let onDone = onDone ?? (target: self, action: #selector(doneButtonTapped))
+
+        let toolbar: UIToolbar = UIToolbar()
+        toolbar.barStyle = .default
+        toolbar.items = [
+            UIBarButtonItem(title: "Cancel", style: .plain, target: onCancel.target, action: onCancel.action),
+            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil),
+            UIBarButtonItem(title: "Done", style: .done, target: onDone.target, action: onDone.action)
+        ]
+        toolbar.sizeToFit()
+
+        self.inputAccessoryView = toolbar
+    }
+    
+    @objc func doneButtonTapped() { self.resignFirstResponder() }
+    @objc func cancelButtonTapped() { self.resignFirstResponder() }
+    
     func setIconRight(_ image:UIImage, _ isAppear:Bool){
         
         let iconView = UIImageView(frame: CGRect(x: -5, y: 0, width: 22, height: 20))
