ad21d1d16d9a8e1a2bcd35b5437ca558425b975e
Jean Paul Borges Manzini <jeanfdq@gmail.com>
2021-01-30 23:28:34 -0300

    Add new extensions

--- Extensions.swift
+++ Extensions.swift
@@ -585,6 +585,8 @@ extension UILabel {
     
     func setTitleAttributeswith(firstTitle:String, firstColor:UIColor, sizeFirstFont:CGFloat, isfirsBold:Bool, secondTitle:String, secondColor:UIColor, sizeSecondFont:CGFloat, isSecondBold:Bool){
         
+        numberOfLines = 2
+
         let attributeText = NSMutableAttributedString(string: firstTitle, attributes: [NSAttributedString.Key.foregroundColor : firstColor, NSAttributedString.Key.font : UIFont.systemFont(ofSize: sizeFirstFont, weight: isfirsBold ? .semibold : .regular)])
         attributeText.append(NSAttributedString(string: secondTitle, attributes: [NSAttributedString.Key.foregroundColor : secondColor, NSAttributedString.Key.font : UIFont.systemFont(ofSize: sizeSecondFont, weight: isSecondBold ? .semibold : .regular)]))
         attributedText = attributeText
